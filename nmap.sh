bdb6476f4956ddf656bf217b73a78eaf3ea324ff
warferik <erik.warfield@gmail.com>
2020-12-23 07:07:28 -0700

    Create nmap.sh

---
+++ nmap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for i in $(cat $1 | grep Disc | awk '{print $6}' | sort -u);
+do
+        ports=""
+        ports=($(grep $i $1 | grep Disc | awk '{print $4}'| tr -d "/tcp"))
+        printf -v joined '%s,' "${ports[@]}"
+        echo "nmap -v -Pn -n -sVC -p ${joined%,} $i >> $i.txt"
+done
